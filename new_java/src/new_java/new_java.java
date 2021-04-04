5e5a051705bbb60d66c37cf0e4e208f19b91de63
Nisha Popshetwar <nishapopshetwar@gmail.com>
2021-04-04 15:32:43 +0530

    first commit
    uploading  all the files

---
+++ new_java/src/new_java/new_java.java
@@ -0,0 +1,22 @@
+package new_java;
+
+public class new_java {
+
+	public static void main(String[] args) {
+		
+				int i=100;
+				long l=i;
+				float f=l;
+				System.out.println("int value"+i);
+				System.out.println("long value"+l);
+				System.out.println("float value"+f);
+				double d=100.04;
+				long m=(long)d;
+				int j=(int)m;
+				System.out.println("double  value"+d);
+				System.out.println("long value"+m);
+				System.out.println("int value"+j);
+			
+	}
+
+}
---
+++ new_java/src/new_java/user.java
@@ -0,0 +1,14 @@
+class Demo
+{
+public static void main(String args[])
+{class Demo
+	{
+	public static void main(String args[])
+	{
+	System.out.println("welcome to java");
+	}
+	}
+System.out.println("welcome to java");
+}
+}
+
